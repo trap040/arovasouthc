663c8743267a97f80f6ac90e7091f27c191cf679
Trap Kid <trapkid040@gmail.com>
2025-03-05 11:40:30 +0300

    social media icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,13 +2,13 @@
 import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMapMarkerAlt, faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
-
+import { faFacebookF, faInstagram } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
   return (
     <footer>
       <div className="bg-nero py-16 px-3">
-        <div className="grid max-w-[1200px] mx-auto gap-8 text-center md:grid-cols-2 md:text-start lg:grid-cols-footer">
+        <div className="grid max-w-[1200px] mx-auto gap-8 text-center md:grid-cols-2 md:text-start lg:grid-cols-[1fr_1fr_1fr_1fr_1fr]">
           {/* Logo & Description */}
           <div className="md:me-2 lg:me-3">
             <Link href="/" className="text-lion font-gilda font-normal text-2xl tracking-[.04em]">
@@ -76,6 +76,30 @@ const Footer = () => {
                 <span className="text-white/50 font-light">arovahotel@gmail.com</span>
               </li>
             </ul>
+          </div>
+          
+          {/* Socials - New separate section with smaller icons */}
+          <div>
+            <h4 className="inline-block font-gilda tracking-[.04em] text-lg text-white capitalize relative after:absolute after:content-[''] after:left-0 after:-bottom-0 after:h-[1px] after:w-full after:bg-coyote pb-1 mb-4">
+              Socials
+            </h4>
+            <div className="flex flex-col items-center md:items-start space-y-4">
+              <Link href="https://facebook.com" target="_blank" rel="noopener noreferrer" 
+                className="flex items-center text-white hover:text-lion transition-colors group">
+                <span className="w-7 h-7 rounded-full bg-lion flex items-center justify-center group-hover:bg-white transition-colors mr-3">
+                  <FontAwesomeIcon icon={faFacebookF} className="text-white group-hover:text-lion w-3 h-3" />
+                </span>
+                <span className="text-white/50 font-light group-hover:text-white/80">Facebook</span>
+              </Link>
+              
+              <Link href="https://instagram.com" target="_blank" rel="noopener noreferrer"
+                className="flex items-center text-white hover:text-lion transition-colors group">
+                <span className="w-7 h-7 rounded-full bg-lion flex items-center justify-center group-hover:bg-white transition-colors mr-3">
+                  <FontAwesomeIcon icon={faInstagram} className="text-white group-hover:text-lion w-3 h-3" />
+                </span>
+                <span className="text-white/50 font-light group-hover:text-white/80">Instagram</span>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
